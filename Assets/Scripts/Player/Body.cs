6af3e63b98bebce02eff19a211ed5eeb9fd83b5e
Kristjan <kristjan.puusepp@tudeng.ut.ee>
2018-04-21 15:26:43 +0300

    if check

--- Assets/Scripts/Player/Body.cs
+++ Assets/Scripts/Player/Body.cs
@@ -7,6 +7,10 @@ public class Body : MonoBehaviour {
 		if (next != null) {
 			next.Move(transform.position);
 		}
+        else
+        {
+            GridSingleton.Set(new Vector2Int((int)transform.position.x, (int)transform.position.y), TileType.EMPTY);
+        }
 		transform.position = pos;
 	}
 
