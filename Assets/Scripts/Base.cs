a7305900ef9734cfab47e43678c2a4b90a073978
Torn <ott.adermann@gmail.com>
2018-04-21 22:14:43 +0300

    things

--- Assets/Scripts/Base.cs
+++ Assets/Scripts/Base.cs
@@ -3,26 +3,9 @@ using UnityEngine.UI;
 
 public class Base : MonoBehaviour {
 	public int hp;
-    public int gold = 0;
-	public int Gold {
-		get {
-			return gold;
-		} set {
-			gold = value;
-			UpdateGoldText();
-		}
-	}
-
-    public Text goldText;
 
 	void Start() {
-        UpdateGoldText();
 		GridSingleton.Instance.Set(new Vector2Int((int) transform.position.x, (int) transform.position.y), TileType.BASE);
 	}
-
-    public void UpdateGoldText()
-    {
-        goldText.text = "Gold: " + gold.ToString();
-    }
 
 }
