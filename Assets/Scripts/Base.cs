e26d9676e147ebe185d711f6e31dc03578207479
Kristjan <kristjan.puusepp@tudeng.ut.ee>
2018-04-21 20:44:32 +0300

    gold

--- Assets/Scripts/Base.cs
+++ Assets/Scripts/Base.cs
@@ -8,6 +8,7 @@ public class Base : MonoBehaviour {
     public Text goldText;
 
 	void Start() {
+        UpdateGoldText();
 		GridSingleton.Instance.Set(new Vector2Int((int) transform.position.x, (int) transform.position.y), TileType.BASE);
 	}
 
