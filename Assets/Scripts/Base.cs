1a76e841fe297e61b4c3a291eebec4f6c6a1065b
Kristjan <kristjan.puusepp@tudeng.ut.ee>
2018-04-21 20:38:35 +0300

    gold text

--- Assets/Scripts/Base.cs
+++ Assets/Scripts/Base.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Base : MonoBehaviour {
 	public int hp;
+    public int gold;
+    public Text goldText;
 
 	void Start() {
 		GridSingleton.Instance.Set(new Vector2Int((int) transform.position.x, (int) transform.position.y), TileType.BASE);
 	}
+
+    public void UpdateGoldText()
+    {
+        goldText.text = "Gold: " + gold.ToString();
+    }
 
 }
