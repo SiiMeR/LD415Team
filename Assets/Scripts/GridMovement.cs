838c3b5389bda6f6ea7f159bd064593cc9844628
Torn <ott.adermann@gmail.com>
2018-04-21 12:32:31 +0300

    new gridmovement

--- Assets/Scripts/GridMovement.cs
+++ Assets/Scripts/GridMovement.cs
@@ -3,35 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GridMovement : MonoBehaviour {
-
-	[SerializeField] private int _tilesPerSecond = 1;
+	[SerializeField] private float _tilesPerSecond = 1;
 	
-	public int TilesPerSecond
-	{
-		get { return _tilesPerSecond; }
-		set { _tilesPerSecond = value; }
-	}
+	public float TilesPerSecond { get; set; }
 
-	private float _moveTimer;
+	private int counter = 0;
+	private int n;
 	
 	public Vector2 MoveDirection { get; set; }
 
-	// Use this for initialization
 	void Start () {
+		n = Mathf.RoundToInt(1 / (Time.fixedDeltaTime * _tilesPerSecond));
 		MoveDirection = Vector2.right;
 	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		_moveTimer += Time.deltaTime;
-	}
 
-	private void FixedUpdate()
-	{
-		if (_moveTimer > 1.0f/_tilesPerSecond)
-		{
-			_moveTimer = 0;
+	void FixedUpdate() {
+		counter++;
+
+		if (counter == n) {
+			counter = 0;
 			transform.Translate(MoveDirection);
 		}
 		
