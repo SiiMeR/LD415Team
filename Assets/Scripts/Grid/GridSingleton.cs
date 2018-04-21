eb0b6922e6ffbc368449bcc9e3c1e84cb041b3f6
siimer <siimraudsep@gmail.com>
2018-04-21 20:11:53 +0300

    broken pathfind

--- Assets/Scripts/Grid/GridSingleton.cs
+++ Assets/Scripts/Grid/GridSingleton.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GridSingleton : Singleton<GridSingleton>
 {
 	[SerializeField] private GameObject bgTile;
 	
-	public Transform StartPosition;
+	//public Transform StartPosition;
 	public LayerMask LayerMask;
 	public Vector2Int gridWorldSize;
 	public float nodeRadius;
@@ -34,10 +35,15 @@ public class GridSingleton : Singleton<GridSingleton>
 
 		grid[colRow.y * gridSizeX + colRow.x].type = type;	
 	}
-	
+
+	public void Set(int col, int row, GridTile tile)
+	{
+		grid[row * gridSizeX + col] = tile;
+	}
 	
 	private void Awake()
 	{
+	//	FinalPath = new List<GridTile>();	
 		nodeDiameter = nodeRadius * 2;  
 		gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
 		
@@ -51,24 +57,27 @@ public class GridSingleton : Singleton<GridSingleton>
 	private void CreateGrid()
 	{
 		
-
-		Vector3 bottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 -
-												  Vector3.up * gridWorldSize.y / 2;
+		Vector3 bottomLeft = transform.position -
+		                     Vector3.right * gridWorldSize.x / 2 -
+							 Vector3.up * gridWorldSize.y / 2;
 		
 		
 		
-		for (var y = 0; y < gridSizeY; y++)
+		for (var x = 0; x < gridSizeX; x++)
 		{
-			for (var x = 0; x < gridSizeX; x++)
+			for (var y = 0; y < gridSizeY; y++)
 			{
-				Vector3 worldPoint = bottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) +
+				Vector3 worldPoint = bottomLeft +
+				                     Vector3.right * (x * nodeDiameter + nodeRadius) +
 				                     Vector3.up * (y * nodeDiameter + nodeRadius);
+				
+//				print(worldPoint);
 
 				//var wall = !Physics.CheckBox(worldPoint, new Vector3(nodeRadius, nodeRadius, nodeRadius), Quaternion.identity, LayerMask);
 				
-				grid[y * gridSizeX + x] = new GridTile(worldPoint, x, y, TileType.EMPTY);
+		    	GridTile tileempty = new GridTile(worldPoint, x, y, TileType.EMPTY);
 				
-				grid[y * gridSizeX + x] = new GridTile(worldPoint, x, y, TileType.EMPTY);
+				Set(x,y, tileempty);
 				if (y % BGUnitsPerTile == 0 && x % BGUnitsPerTile == 0)
 				{
 					
@@ -122,6 +131,7 @@ public class GridSingleton : Singleton<GridSingleton>
 					}
 				}
 
+		
 
 				Color c = Gizmos.color;
 				c.a = 0.99f;
@@ -129,5 +139,85 @@ public class GridSingleton : Singleton<GridSingleton>
 				Gizmos.DrawCube(new Vector3(node.Position.x -offset, node.Position.y -offset), new Vector3(0.7f,0.7f,0.7f) * (nodeDiameter - Distance));
 			}
 		}
+	}
+
+	public GridTile TileFromWorldPos(Vector3 startPos)
+	{
+		float xpoint = ((startPos.x + (float)gridWorldSize.x/ 2) / (float)gridWorldSize.x);
+
+		float ypoint = ((startPos.y+ (float)gridWorldSize.y/ 2) / (float)gridWorldSize.y);
+
+	
+		
+//		print(xpoint + " ss " + ypoint);
+		xpoint = Mathf.Clamp01(xpoint);
+		ypoint = Mathf.Clamp01(ypoint);
+
+		int x = Mathf.RoundToInt((gridSizeX - 1) * xpoint);
+		int y = Mathf.RoundToInt((gridSizeY - 1) * ypoint);
+
+		return Get(x, y);
+
+	}
+
+	public List<GridTile> getNeighbours(GridTile currentTile)
+	{
+		List<GridTile> neighbours = new List<GridTile>();
+
+		int xCheck;
+		int yCheck;
+
+		
+		// right nb
+		xCheck = currentTile.gridX + 1;
+		yCheck = currentTile.gridY;
+
+		if (xCheck >= 0 && xCheck < gridSizeX)
+		{
+			if (yCheck >= 0 && yCheck < gridSizeY)
+			{
+				neighbours.Add(Get(xCheck, yCheck));
+			}
+		}
+		
+		// left nb
+		xCheck = currentTile.gridX - 1;
+		yCheck = currentTile.gridY;
+
+		if (xCheck >= 0 && xCheck < gridSizeX)
+		{
+			if (yCheck >= 0 && yCheck < gridSizeY)
+			{
+				neighbours.Add(Get(xCheck, yCheck));
+			}
+		}
+
+		
+		// top nb
+		xCheck = currentTile.gridX;
+		yCheck = currentTile.gridY + 1;
+
+		if (xCheck >= 0 && xCheck < gridSizeX)
+		{
+			if (yCheck >= 0 && yCheck < gridSizeY)
+			{
+				neighbours.Add(Get(xCheck, yCheck));
+			}
+		}
+		
+		
+		// bot nb
+		xCheck = currentTile.gridX;
+		yCheck = currentTile.gridY -1;
+
+		if (xCheck >= 0 && xCheck < gridSizeX)
+		{
+			if (yCheck >= 0 && yCheck < gridSizeY)
+			{
+				neighbours.Add(Get(xCheck, yCheck));
+			}
+		}
+
+		return neighbours;
 	}
 }
--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -1,16 +1,116 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking.Types;
 
-public class Pathfinder : MonoBehaviour {
+public class Pathfinder : MonoBehaviour
+{
 
+	private GridSingleton grid;
+
+	public Transform startPos;
+
+	public Transform endPos;
+	
+	 
 	// Use this for initialization
-	void Start () {
-		
+	void Awake ()
+	{
+		grid = GetComponent<GridSingleton>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+		FindPath(startPos.position, endPos.position);
+	}
+
+	private void FindPath(Vector3 startPos, Vector3 endPos)
+	{
+
+		GridTile startTile = grid.TileFromWorldPos(startPos);
+		GridTile endTile = grid.TileFromWorldPos(endPos);
 		
+//		print(endTile.gridX + " " + endTile.gridY);
+//		print(startTile.gridX + " " + startTile.gridY);
+		
+		List<GridTile> openList = new List<GridTile>();
+		HashSet<GridTile> closedList = new HashSet<GridTile>();
+		
+		openList.Add(startTile);
+
+		while (openList.Count > 0)
+		{
+			GridTile currentTile = openList[0];
+			for (int i = 1; i < openList.Count; i++)
+			{
+				if (openList[i].FCost < currentTile.FCost ||
+				    openList[i].FCost == currentTile.FCost && 
+				    openList[i].hCost < currentTile.hCost)
+				{
+					currentTile = openList[i];
+				}
+
+			}
+
+			openList.Remove(currentTile);
+			closedList.Add(currentTile);
+
+			if (currentTile == endTile)
+			{
+				GetFinalPath(startTile, endTile);
+			}
+
+			foreach (var neighbour in grid.getNeighbours(currentTile))
+			{
+				if (neighbour.type == TileType.SNAKE || closedList.Contains(neighbour))
+				{
+					continue;
+				
+				}
+
+				int movecost = currentTile.gCost + GetManHTDist(currentTile, neighbour);
+
+				if (movecost < neighbour.gCost || !openList.Contains(neighbour))
+				{
+					neighbour.gCost = movecost;
+					neighbour.hCost = GetManHTDist(neighbour, endTile);
+					neighbour.Parent = currentTile;
+
+					if (!openList.Contains(neighbour))
+					{
+						openList.Add(neighbour);
+					}
+				}
+			}
+		}
+	}
+
+	private int GetManHTDist(GridTile tile1, GridTile tile2)
+	{
+		int ix = Mathf.Abs(tile1.gridX - tile2.gridX);
+		int iy = Mathf.Abs(tile1.gridY - tile2.gridY);
+
+		return ix + iy;
+	}
+
+	private void GetFinalPath(GridTile startTile, GridTile endTile)
+	{
+		List<GridTile> finalPath = new List<GridTile>();
+		GridTile currentTile = endTile;
+
+		while (currentTile != startTile)
+		{
+			finalPath.Add(currentTile);
+			currentTile = currentTile.Parent;
+			
+		}
+		
+		finalPath.Reverse();
+
+		grid.FinalPath = finalPath;
+		
+		
+//		print(finalPath.Count);
 	}
 }
