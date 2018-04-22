3ad4f3bcc8f8d9ea3d934aa311b6387057dd498c
siimer <siimraudsep@gmail.com>
2018-04-22 14:05:53 +0300

    new ui menu

---
+++ Assets/MainMenu/Scripts/RotateCog.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCog : MonoBehaviour {
+    public float direction = 1;
+    public GameObject[] gears;
+    public float[] gearSpeeds;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float speed = direction;
+        for (int i = 0; i < gears.Length; i++)
+        {
+            gears[i].transform.Rotate(new Vector3(0, 0, speed * gearSpeeds[i]));
+                speed = -speed;
+        }
+    }
+}
